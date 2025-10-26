b3e07f68b0d666983e28da70711bb2e9643ccf2c
Julian Marvick Fua Oliveros <julian.marvick.oliveros@gmail.com>
2025-10-26 21:47:37 +0900

    Print received HTTP request

--- src/main.c
+++ src/main.c
@@ -7,14 +7,17 @@
 #include <errno.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 1024
+
 int main() {
+	char buffer[BUFFER_SIZE];
 	// Disable output buffering
 	setbuf(stdout, NULL);
  	setbuf(stderr, NULL);
 
 	printf("HTTP Server started\n");
 	
-	int server_fd, client_addr_len;
+	int server_fd, client_addr_len, clientSocket;
 	struct sockaddr_in client_addr;
 	
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -50,11 +53,35 @@ int main() {
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
 	
-	int id = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	printf("Client connected\n");
-	send(id, "HTTP/1.1 200 OK\r\n\r\n", 19, 0);
-	
-	close(server_fd);
+	while (1) {
+        // Accept a client connection
+		clientSocket = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+		if (clientSocket < 0) {
+			perror("Error accepting connection");
+			continue;
+		}
+
+        printf("Client connected from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+        // Read the HTTP request
+        ssize_t bytes_received = recv(clientSocket, buffer, BUFFER_SIZE - 1, 0);
+        if (bytes_received > 0) {
+            buffer[bytes_received] = '\0'; // Null-terminate the received data
+            printf("Received HTTP Request:\n%s\n", buffer);
+
+            // Send a simple HTTP response
+            const char *http_response = "HTTP/1.1 200 OK\r\n\r\n";
+            send(clientSocket, http_response, strlen(http_response), 0);
+        } else if (bytes_received == 0) {
+            printf("Client disconnected.\n");
+        } else {
+            perror("Error receiving data");
+        }
+
+		// Close the client socket
+		close(clientSocket);
+		close(server_fd);
+	}
 
 	return 0;
 }
